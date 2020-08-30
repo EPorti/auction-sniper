e2217ef5bcf71ae756436b1d754f370faf197e73
Andriy Holovko <andriy.holovko@gmail.com>
2020-08-30 12:40:23 +0300

    test: XMPPAuction can create a Chat and attach a listener

---
+++ src/intTest/java/auctionsniper/xmpp/XMPPAuctionTest.java
@@ -0,0 +1,63 @@
+package auctionsniper.xmpp;
+
+import auctionsniper.*;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+public class XMPPAuctionTest {
+    private final FakeAuctionServer auctionServer = new FakeAuctionServer("item-54321");
+    private XMPPConnection connection;
+
+    @Before
+    public void createConnection() throws XMPPException {
+        connection = new XMPPConnection(FakeAuctionServer.XMPP_HOSTNAME);
+        connection.connect();
+        connection.login(ApplicationRunner.SNIPER_ID, ApplicationRunner.SNIPER_PASSWORD, Main.AUCTION_RESOURCE);
+    }
+
+    @Before
+    public void startTheAuction() throws XMPPException {
+        auctionServer.startSellingItem();
+    }
+
+    @After
+    public void stopAuction() {
+        auctionServer.stop();
+    }
+
+    @Test
+    public void receivesEventsFromAuctionServerAfterJoining() throws Exception {
+        CountDownLatch auctionWasClosed = new CountDownLatch(1);
+
+        Auction auction = new XMPPAuction(connection, auctionServer.getItemId());
+        auction.addAuctionEventListener(auctionClosedListener(auctionWasClosed));
+        auction.join();
+
+        auctionServer.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auctionServer.announceClosed();
+
+        assertTrue("should have been closed", auctionWasClosed.await(2, TimeUnit.SECONDS));
+    }
+
+    private AuctionEventListener auctionClosedListener(CountDownLatch auctionWasClosed) {
+        return new AuctionEventListener() {
+            @Override
+            public void auctionClosed() {
+                auctionWasClosed.countDown();
+            }
+
+            @Override
+            public void currentPrice(int price, int increment, PriceSource source) {
+                // not implemented
+            }
+        };
+    }
+}
